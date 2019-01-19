947c91cd1322df04b81123b4b6abc94f48a426ed
shiranAziz <shiran.aziz@gmail.com>
2019-01-19 16:59:42 +0200

    testing commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
 
 public class Main {
 
     public static void main(String[] args) {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         System.out.println(Core.VERSION);
+        Mat mat = Mat.eye(3,3, CvType.CV_32F);
+        System.out.println(mat.dump());
     }
 }
